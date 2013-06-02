82d93ed11dfdc75e9b790cd38f2558679bb88128
Claudio Medeiros <klawdyo@gmail.com>
2013-06-02 00:23:25 -0300

    Some changes in Band

--- Video/band.php
+++ Video/band.php
@@ -4,7 +4,7 @@
     COPYRIGHT
     ---------------------------------------------------------------------------
     @author José Cláudio Medeiros de Lima <klawdyo@gmail.com>
-    @version 0.1 16/09/2012
+    @version 0.3 02/06/2013
 
     ---------------------------------------------------------------------------
     DESCRIPTION
@@ -19,10 +19,13 @@ class band{
       *
       * @version
       *     0.1 16/09/2012 Initial
+      *     0.2 28/05/2013 Band agora usando os vídeos do UOL
+      *     0.2 02/06/2013 Modificada a forma de adquirir o html, pegando somente
+      *         o necessário com o método Video::getContents
       */
     public static function details($id, $url){
-        $data = Video::getContents($url);
-        pr($data);
+        $data = Video::openGraph(null, Video::getContents($url, 2048));
+        
         #image
         //http://thumb.mais.uol.com.br/14471367-large.jpg?ver=2
         
@@ -31,7 +34,7 @@ class band{
         
         #player
         //'player' => 'http://player.mais.uol.com.br/embed_v2.swf?tv=2&mediaId=' . $id,
-        return array();
+        
         return array(
             'title' => $data['title'],
             'image' => 'http://thumb.mais.uol.com.br/' . $id . '-large.jpg?ver=2',
